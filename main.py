1027906b9698962a92d2368023a03f616d2a8a97
Unpei2 <r.ho5@share.epsb.ca>
2023-01-12 13:56:51 -0700

    done

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
 import random
 loop = True
 while loop:
-
     print("\nDice Roll Simulator Menu \n1. Roll Dice Once\n2. Roll Dice 5 Times\n3. Roll Dice 'n' Times \n4. Roll Dice until Snake Eyes\n5. Exit")
     select = int(input("\nSelect an option (1-5):"))
     if select == 1:
@@ -12,21 +11,37 @@ while loop:
         randnum2 = random.randrange(1, 7)
         sum = randnum1 + randnum2
         print(str(randnum1) + "," + str(randnum2) + " (sum: " + str(sum) + ")")
-
     elif select == 2:
-        x = 5
+        x = 0
         while x < 5:
             randnum1 = random.randrange(1, 7)
-            print(randnum1)
             randnum2 = random.randrange(1, 7)
             sum = randnum1 + randnum2
-            print(str(randnum1))
-        print("ok")
+            print(str(randnum1) + "," + str(randnum2) +
+                  " (sum: " + str(sum) + ")")
+            x += 1
     elif select == 3:
-        print("ah")
+        x = 0
+        number = int(input("How many rolls would you like? "))
+        while x < number:
+            randnum1 = random.randrange(1, 7)
+            randnum2 = random.randrange(1, 7)
+            sum = randnum1 + randnum2
+            print(str(randnum1) + "," + str(randnum2) +
+                  " (sum: " + str(sum) + ")")
+            x += 1
     elif select == 4:
-
-        print("ahHHHHHHHHHHHHHHHHHHHH!!!!!!asddsdddd")
+        x = 0
+        snake = False
+        while snake == False:
+            randnum1 = random.randrange(1, 7)
+            randnum2 = random.randrange(1, 7)
+            sum = randnum1 + randnum2
+            print(str(randnum1) + "," + str(randnum2) +
+                  " (sum: " + str(sum) + ")")
+            x += 1
+            if randnum1 and randnum2 == 1:
+                snake = True
+                print("SNAKE EYES! It took " + str(x) + " to get snake eyes.")
     else:
-        print("asdasd")
         loop = False

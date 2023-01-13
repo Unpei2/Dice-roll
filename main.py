5cdbfc253dd9c320b8367d4fa1a9379620b900d5
Unpei2 <r.ho5@share.epsb.ca>
2023-01-13 10:50:33 -0700

    Update main.py

--- main.py
+++ main.py
@@ -1,34 +1,32 @@
 import random
 loop = True
+
+
+def function():
+    randnum1 = 0
+    randnum2 = 0
+    sum = 0
+    randnum1 = random.randrange(1, 7)
+    randnum2 = random.randrange(1, 7)
+    sum = randnum1 + randnum2
+    print(str(randnum1) + "," + str(randnum2) + " (sum: " + str(sum) + ")")
+
+
 while loop:
     print("\nDice Roll Simulator Menu \n1. Roll Dice Once\n2. Roll Dice 5 Times\n3. Roll Dice 'n' Times \n4. Roll Dice until Snake Eyes\n5. Exit")
     select = int(input("\nSelect an option (1-5):"))
     if select == 1:
-        randnum1 = 0
-        randnum2 = 0
-        sum = 0
-        randnum1 = random.randrange(1, 7)
-        randnum2 = random.randrange(1, 7)
-        sum = randnum1 + randnum2
-        print(str(randnum1) + "," + str(randnum2) + " (sum: " + str(sum) + ")")
+        function()
     elif select == 2:
         x = 0
         while x < 5:
-            randnum1 = random.randrange(1, 7)
-            randnum2 = random.randrange(1, 7)
-            sum = randnum1 + randnum2
-            print(str(randnum1) + "," + str(randnum2) +
-                  " (sum: " + str(sum) + ")")
+            function()
             x += 1
     elif select == 3:
         x = 0
         number = int(input("How many rolls would you like? "))
         while x < number:
-            randnum1 = random.randrange(1, 7)
-            randnum2 = random.randrange(1, 7)
-            sum = randnum1 + randnum2
-            print(str(randnum1) + "," + str(randnum2) +
-                  " (sum: " + str(sum) + ")")
+            function()
             x += 1
     elif select == 4:
         x = 0
